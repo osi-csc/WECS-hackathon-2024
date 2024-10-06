434f12fc0490493ed449659f48fcc571fed84d0f
Rosita <rositaliu.bc@gmail.com>
2024-10-06 15:03:26 -0700

    Update forms.py
    added transactionaddform

--- codeher/financeapp/forms.py
+++ codeher/financeapp/forms.py
@@ -1,7 +1,7 @@
 # financeapp/forms.py
 
 from django import forms
-from .models import User, Goals, Budgets
+from .models import User, Goals, Transactions, Budgets
 
 class UserRegistrationForm(forms.ModelForm):
     class Meta:
@@ -13,3 +13,9 @@ class UserRegistrationForm(forms.ModelForm):
 class UserLoginForm(forms.Form):
     username = forms.CharField(max_length=40)
     userPassword = forms.CharField(widget=forms.PasswordInput())
+
+class TransactionAddForm(forms.ModelForm):
+    class Meta:
+        model=Transaction
+        fields=['TransactionType','Category','amount']
+
