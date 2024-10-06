fa887e78eb66bd82e1717113fe090c8a95dabce8
Eniola Afolaranmi <eniola.afolaranmi@gmail.com>
2024-10-06 11:08:24 -0700

    Update finance-database.sql

--- finance-database.sql
+++ finance-database.sql
@@ -1,1 +1,73 @@
+CREATE TABLE IF NOT EXISTS Users (
+  userID SERIAL PRIMARY KEY,
+  username VARCHAR(40),
+  password VARCHAR(40),
+  startDate DATE
+);
+
+CREATE TABLE IF NOT EXISTS TransactionTypes (
+  transactionTypeID SERIAL PRIMARY KEY,
+  transactionTypeName VARCHAR(70) UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS Categories (
+  categoryID SERIAL PRIMARY KEY,
+  categoryName VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS Transactions (
+  transactionID SERIAL PRIMARY KEY,
+  type VARCHAR(70) REFERENCES TransactionTypes(transactionTypeName),
+  categoryID INTEGER REFERENCES Categories(categoryID),
+  user INTEGER REFERENCES Users(userID),
+  amount DECIMAL(15, 2),
+  transactionDate TIMESTAMP,
+  transactionNote TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Budgets (
+  budgetID SERIAL PRIMARY KEY,
+  userID INTEGER REFERENCES Users(userID),
+  categoryID INTEGER REFERENCES Categories(categoryID),
+  amount DECIMAL(15, 2),
+  startDate DATE,
+  endDate DATE
+);
+
+CREATE TABLE IF NOT EXISTS Goals (
+  goalID SERIAL PRIMARY KEY,
+  userID INTEGER REFERENCES Users(userID),
+  goalName VARCHAR(70),
+  targetAmount DECIMAL(15, 2),
+  curentAmount DECIMAL(15, 2) DEFAULT 0,
+  goalEndDate DATE  
+  );
+
+CREATE TABLE IF NOT EXISTS Bills (
+  billID SERIAL PRIMARY KEY,
+  userID INTEGER REFERENCES Users(userID),
+  categoryID INTEGER REFERENCES Categories(categoryID),
+  amount DECIMAL(15,2),
+  interval VARCHAR(15) CHECK (interval IN ('daily', 'weekly', 'monthly', 'yearly'))
+  intervalFrequency INTEGER,
+  nextPayDay DATE
+  );
+
+CREATE TABLE IF NOT EXISTS Income (
+  incomeID SERIAL PRIMARY KEY,
+  userID INTEGER REFERENCES Users(userID),
+  source VARCHAR(70),
+  amount DECIMAL(15, 2),
+  incomeDate TIMESTAMP,
+  incomeNote TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Debts (
+  debtID SERIAL PRIMARY KEY,
+  userID INTEGER REFERENCES Users(userID),
+  debtAmount DECIMAL(15, 2),
+  dueDate DATE,
+  interestRate DECIMAL(5, 2),
+  remainingBalance DECIMAL(15, 2)
+);
 
