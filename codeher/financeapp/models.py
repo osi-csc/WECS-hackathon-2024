af8bd9d2d26a393411ec1bbf25436322acae39f7
Rosita <rositaliu.bc@gmail.com>
2024-10-06 13:24:50 -0700

    Update models.py

--- codeher/financeapp/models.py
+++ codeher/financeapp/models.py
@@ -6,5 +6,18 @@ class User(models.Model):
     userPassword = models.CharField(max_length=40)
     startDate = models.DateField(auto_now_add=True )
 
+class TransactionTypes(models.Model): 
+    transactionTypeID = models.AutoField(primary_key=True)
+    transactionTypeName = models.CharField(max_length=70)
+    
+class Categories (models.Model):
+    categoryID = model.AutoField(primary_key=true)
+    categoryName = models.CharField(max_length=50)
+    
+class Transactions(models.Model):
+    transactionID =models.AutoField(primary_key=True)
+    transactionType= models.transactionType()#foriegn key to TransactionTypeName from TransactionTypes
+
+class   
     def __str__(self):
         return self.username
